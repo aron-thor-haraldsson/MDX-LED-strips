459fd2a11392d8780a65d7a1f3d6cb80a17f7ff9
AH1534 <ah1534@live.mdx.ac.uk>
2016-11-12 17:01:10 +0000

    adds rgb255 file
        the function within handles converting hls to rgb
        so the value is ready to be sent to an LED

---
+++ rgb255.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+#this file contains the function hls1_to_rgb255
+#it converts HLS (Hue/Lightness/Saturation) color format to RGB (Red/Green/Blue)
+
+#uses the hls_to_rgb function imported from colorsys
+
+#parameter: tuple or list of 3 floating point numbers (values ranging from 0 to 1)
+#return: a tuple of 3 integers (values ranging from 0 to 255)
+
+import colorsys
+
+def hls1_to_rgb255(hls1):
+    rgb1 = colorsys.hls_to_rgb(hls1[0], hls1[1], hls1[2])
+    rgb255 = (int(rgb1[0] * 255), int(rgb1[1] * 255), int(rgb1[2] * 255))
+    return rgb255
+	
