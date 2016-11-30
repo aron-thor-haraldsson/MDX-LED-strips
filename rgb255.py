602e50b2cb0b15946384fc38efc3fb721eef6f66
AH1534 <ah1534@live.mdx.ac.uk>
2016-11-30 13:38:48 +0000

    deletes the rgb255.py file because it is not needed

--- rgb255.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import colorsys
-
-# This function converts HLS (Hue/Lightness/Saturation) color format to RGB (Red/Green/Blue).
-# parameter: tuple or list of 3 floating point numbers (values ranging from 0 to 1)
-# return: a tuple of 3 integers (values ranging from 0 to 255)
-
-
