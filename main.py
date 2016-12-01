c2cd1e8c4ae74908d42dfc2caffb38a20c22abce
AH1534 <ah1534@live.mdx.ac.uk>
2016-12-01 12:06:32 +0000

    updated comments

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ import led_classes as led
 import os
 
 
-def update_leds():
+def update_leds(leds_array):
     for q in range (len(leds)):
         if leds[q].update() == 0:
             
             leds[q].set_current(leds[q].get_target())
-            leds[q].set_target(led.get_base_colour(), "linear")
+            leds[q].set_target(leds.get_base_colour(), "linear")
             
+
 def prepare_wall_movement(direction, speed):
-    
     if direction == "diagonal":
         wall.set_current([0, 0, 0])
         wall.set_proximity(120)
@@ -31,19 +31,21 @@ def prepare_wall_movement(direction, speed):
         wall.set_target([0, 6000, 0], speed)
        
 
-
+# Creating instances of objects
+# and filling in their default values
 leds = [led.Led() for i in range(0,360)]
 wall = led.Wall()
- 
 led.localize_leds(leds, 1, (2000, 4000, 1000))
 led.localize_leds(leds, 2, (2000, 4000, 2000))
 led.localize_leds(leds, 3, (2000, 4000, 3000))
 led.localize_leds(leds, 4, (2000, 2000, 1000))
 led.localize_leds(leds, 5, (2000, 2000, 2000))
 led.localize_leds(leds, 6, (2000, 2000, 3000))
-
 led.set_base_colour([1.0, 0.0, 1.0], leds)
 
+# This is the heart of the program, allowing any complicated movement
+# of an imaginary "wall" of light to pass through the LEDs
+# from various angles.
 def perform_sweep(direction, speed, colour = color_names.red()):
     print color_names.red()
     prepare_wall_movement(direction, speed)
@@ -58,9 +60,9 @@ def perform_sweep(direction, speed, colour = color_names.red()):
         led.display_on_fadecandy(leds)
         time.sleep(0.1)
         
+# This performs a set animation displaying fade
 def perform_fade():
     for n in range(1000):
-        print n
         for i in range (len(leds)):
             leds[i].set_current([1.0 + n*0.003 + i*0.003, 0.5, 1.0])
             #print [1.0-n*0.01-i*0.01, 1.0-n*0.01-i*0.01, 1-n*0.01-i*0.01]
